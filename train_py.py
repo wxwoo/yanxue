75a03ddfe319514d288a03bc649e0e858ae0e03a
wxwoo <walkwoo@163.com>
2023-08-14 20:57:42 +0800

    On branch main Changes to be committed: 	modified:   train_py.py

--- train_py.py
+++ train_py.py
@@ -1,4 +1,157 @@
 
+import torch
+import torch.nn as nn
+from torch.optim import Adam
+import torch.nn.functional as F
+from random import randint
+import numpy as np
+# import subprocess
+# import multiprocessing
+# import concurrent.futures
+from time import time
+from math import sqrt
+
+CHANNEL = 32
+BLOCKNUM = 10
+BOARDSIZE = 8
+BATCH = 50
+EPOCHS = 20
+DATASIZE = 7200
+DATAUSE = 2000
+ROUNDLIMIT = 500
+PROCESS = 3
+OUTPUT_INFO = 0
+
+class resBlock(nn.Module):
+    def __init__(self, x):
+        super(resBlock, self).__init__()
+        self.resBlock = nn.Sequential(
+            nn.Conv2d(x, x, kernel_size=3, stride=1, padding=1, bias=False),
+            nn.BatchNorm2d(x),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(x, x, kernel_size=3, stride=1, padding=1, bias=False),
+            nn.BatchNorm2d(x)
+        )
+        self.relu = nn.ReLU(inplace=True)
+            
+    def forward(self, x):
+        shortCut = x
+        out = self.resBlock(x)
+        out += shortCut
+        out = self.relu(out)
+        return out
+    
+class resCNN(nn.Module):
+    def __init__(self):
+        super(resCNN, self).__init__()
+        self.input = nn.Sequential(
+            nn.Conv2d(3, CHANNEL, kernel_size=3, stride=1, padding=1, bias=False),
+            nn.BatchNorm2d(CHANNEL),
+            nn.ReLU(inplace=True)
+        )
+        self.resnet = nn.Sequential()
+        for i in range(BLOCKNUM):
+            self.resnet.add_module(str(i),resBlock(CHANNEL))
+        
+        self.ph = nn.Sequential(
+            nn.Conv2d(CHANNEL, 2, kernel_size=1, stride=1, bias=False),
+            nn.BatchNorm2d(2),
+            nn.ReLU(inplace=True),
+            nn.Flatten(),
+            nn.Linear(BOARDSIZE*BOARDSIZE*2, BOARDSIZE*BOARDSIZE),
+            # nn.Softmax(dim=1)
+        )
+        self.vh = nn.Sequential(
+            nn.Conv2d(CHANNEL, 1, kernel_size=1, stride=1, bias=False),
+            nn.BatchNorm2d(1),
+            nn.ReLU(inplace=True),
+            nn.Flatten(),
+            nn.Linear(BOARDSIZE*BOARDSIZE, CHANNEL),
+            nn.ReLU(inplace=True),
+            nn.Linear(CHANNEL, 1),
+            nn.Tanh()
+        )
+        
+    def forward(self, x):
+        model = self.input(x)
+        model = self.resnet(model)
+        p = self.ph(model)
+        v = self.vh(model)
+        return p, v
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+cnn = resCNN()
+cnn.load_state_dict(torch.load(r'./rescnn.pth'))
+cnn.to(device)
+optimizer = Adam(cnn.parameters(), weight_decay=1e-4)
+
+stateData = torch.zeros(DATASIZE, 3, 8, 8, dtype=float)
+policyData = torch.zeros(DATASIZE, 64, dtype=float)
+valueData = torch.zeros(DATASIZE, 1, dtype=float)
+policyLossFunc = nn.CrossEntropyLoss()
+valueLossFunc = nn.MSELoss()
+
+def calc(cood):
+    return cood[0] * BOARDSIZE + cood[1]
+
+def lossFunction(policyOutput, valueOutput, policyTarget, valueTarget):
+    policyLoss = policyLossFunc(policyOutput, policyTarget)
+    valueLoss = valueLossFunc(valueOutput, valueTarget)
+    return policyLoss + valueLoss
+
+def train():
+    cnn.train()
+    use = torch.zeros(DATASIZE)
+    inputData = torch.zeros(DATAUSE,3,8,8)
+    policyTargetData = torch.zeros(DATAUSE,64)
+    valueTargetData = torch.zeros(DATAUSE,1)
+    
+    i = 0
+    while i < DATAUSE:
+        x = randint(0, DATASIZE - 1)
+        if use[x] == 1:
+            continue
+        inputData[i] = stateData[x]
+        policyTargetData[i] = policyData[x]
+        valueTargetData[i] = valueData[x]
+        use[x] = 1
+        i += 1
+        
+    optimizer.zero_grad()
+    for i in range(EPOCHS):
+        policyLossAvg = 0.0
+        valueLossAvg = 0.0
+
+        if OUTPUT_INFO:
+            print(f'epoch {i+1}:')
+
+        for j in range(0, DATAUSE, BATCH):
+
+            input = inputData[j:j+BATCH]
+            policyTarget = policyTargetData[j:j+BATCH]
+            valueTarget = valueTargetData[j:j+BATCH]
+
+            policyOutput, valueOutput = cnn(input.to(device))
+
+            policyLoss = policyLossFunc(policyOutput, policyTarget.to(device))
+            valueLoss = valueLossFunc(valueOutput, valueTarget.to(device))
+            loss = policyLoss + valueLoss
+            policyLossAvg += float(policyLoss)
+            valueLossAvg += float(valueLoss)
+
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+        if OUTPUT_INFO:
+            print(f'    policy loss: {policyLossAvg / (DATAUSE / BATCH)}')
+            print(f'    value loss: {valueLossAvg / (DATAUSE / BATCH)}')
+            print(f'    total loss: {(policyLossAvg + valueLossAvg) / (DATAUSE / BATCH)}')
+        
+    torch.save(cnn.state_dict(), r'./rescnn.pth')
+
+
 class GameState:
     def __init__(self):
         self.board = np.zeros((8, 8), dtype=np.int8) # 0 ~ 7
@@ -228,6 +381,29 @@ def gen_py():
             currentPlayer = -currentPlayer
         
         valueData[lst:cnt] = c_state.getWinner()
-        print(f'{cnt} / {DATASIZE}\r', end='')
-        
-    print('')
+        if OUTPUT_INFO:
+            print(f'{cnt} / {DATASIZE}\r', end='')
+
+    if OUTPUT_INFO:
+        print('')
+
+
+if __name__ == '__main__':
+    np.set_printoptions(suppress=True, precision=7)
+    # multiprocessing.freeze_support()
+    times = 0
+    while 1 :
+        if OUTPUT_INFO:
+            print(f'iteration {times}:')
+            print('self-matching:')
+
+        gen_py()
+        # gen_cpp()
+        # gen_mainProcess() # in train.py
+
+        if OUTPUT_INFO:
+            print('train start:')
+
+        train()
+        # archivePath = 'D:/Desktop/yanxue/rescnn_archive/rescnn-iteration' + str(times) +'.pth'
+        # torch.save(cnn.state_dict(), archivePath)
